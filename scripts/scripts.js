706a7073610e5f6634f06e671cac8b819a7d0e51
Faiza Husain and Josh Rochon <faiza+josh@>
2018-01-23 14:03:31 -0800

    add new branch for refactorization

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,59 +1,29 @@
 //BUSINESS LOGIC
 
 //CONSTRUCTOR
-function Profile (name, movie, date, age) {
-  this.userName = name;
-  this.movie = movie;
-  this.date = date;
-  this.age = age;
+function movieTicket () {
+
 }
 
+function Movie (movieName, newRelease) {
+  this.movieName = name;
+  this.newRelease = release;
+}
 
+var movieArray = [
+  new Movie ("The Last Jedi", true),
+  new Movie ("Jumanji", true),
+  new Movie ("Saw", false),
+  new Movie ("Wonder Woman", false),
+  new Movie ("Lady Bird", true)
+]
 
-function ageVerify(){
-
-  if(userProfile.movie==="Saw" && userProfile.age <17){
-    $(".sorry").show();
-  }else if (userProfile.age >= 50){
-    $(".senior").show();
-  } else if (userProfile.age >= 18) {
-    $(".young-adult").show();
-  } else {
-    $(".child").show();
-  }
 
 };
 
 
 //USER INTERFACE LOGIC
-$(document).ready(function(){
-  $("#form1").submit(function(event){
-    event.preventDefault();
 
-
-    var userName = $("#userName").val();
-    var movie = $("#movie").val();
-    var date = $("#date").val();
-    var age = parseInt($("#age").val());
-
-    userProfile = new Profile (userName, movie, date, age);
-
-    console.log(userProfile);
-
-    //console.log(userProfile.userNamename);
-
-    // console.log(userName);
-    // console.log(movie);
-    // console.log(date);
-    // console.log(age);
-
-    $(".name").text(userProfile.userName);
-    $(".movie").text(userProfile.movie);
-    $(".date").text(userProfile.date);
-
-    $("#summary").show();
-
-    ageVerify();
   });
 
 
