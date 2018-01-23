86aa0fa888a14a8a3c7903bbfbd28586cbad0053
Faiza Husain and Josh Rochon <josh@>
2018-01-23 11:07:38 -0800

    modifies html and adds more front-end logic to JS

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,46 @@
+//BUSINESS LOGIC
+//CONSTRUCTOR
+
+function Profile (name, movie, date, age) {
+  this.userName = name;
+  this.movie = movie;
+  this.date = date;
+  this.age = age;
+}
+
+function ageVer(){
+  if (age < 17){
+    $(".sorry").show();
+  }
+}
+
+
+//USER INTERFACE LOGIC
 $(document).ready(function(){
   $("#form1").submit(function(event){
     event.preventDefault();
 
+
+    var userName = $("#userName").val();
     var movie = $("#movie").val();
     var date = $("#date").val();
-    var age = $("#age").val();
+    var age = parseInt($("#age").val());
 
+    var userProfile = new Profile (userName, movie, date, age);
+
+    console.log(userProfile);
+
+    //console.log(userProfile.userNamename);
+
+    // console.log(userName);
+    // console.log(movie);
+    // console.log(date);
+    // console.log(age);
+
+    $(".name").text(userProfile.userName);
+    $(".movie").text(userProfile.movie);
+    $(".date").text(userProfile.date);
+
+    $("#initially-hidden").show();
   });
 });
