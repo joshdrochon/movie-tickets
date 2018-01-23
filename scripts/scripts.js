f4a42ca90815ccf4607a7bf32dad2c0735214865
Faiza Husain and Josh Rochon <josh@>
2018-01-23 14:29:35 -0800

    new bracnh to practice arrays and loops

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,13 +1,25 @@
 //BUSINESS LOGIC
+var myMovie;
 
-//CONSTRUCTOR
-function movieTicket () {
+// run a loop on the moviesArray to determine if the movie selected in the form is the same as the user movie
+function getMovie (userMovie) {
+  movieArray.forEach(function(movie){
+    if(movie.movieName === userMovie) {
+      myMovie = movie;;
+    };
+  });
+};
 
+function movieTicket (movieName, timeOfDay, userAge) {
+  this.movieName = name;
+  this.timeOfDay = time;
+  this.userAge = age;
+  this.price = 0;
 }
 
 function Movie (movieName, newRelease) {
-  this.movieName = name;
-  this.newRelease = release;
+  this.movieName = movieName;
+  this.newRelease = newRelease;
 }
 
 var movieArray = [
@@ -16,15 +28,15 @@ var movieArray = [
   new Movie ("Saw", false),
   new Movie ("Wonder Woman", false),
   new Movie ("Lady Bird", true)
-]
+];
 
+function fillMovieForm() {
+  moviesArray.forEach(function (movie) {
+    $("#movies").append("<option> " + movieArray[i].movieName + "</option>");
+    i++;
+  });
+}
 
-};
 
 
 //USER INTERFACE LOGIC
-
-  });
-
-
-});
